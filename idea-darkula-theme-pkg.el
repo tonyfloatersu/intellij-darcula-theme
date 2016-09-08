6ca8e1751e92b10014525899ea085e60e42a94cd
tonyfloatersu <tonyfloater@gmail.com>
2016-09-08 17:08:15 +0800

    Add files via upload

---
+++ idea-darkula-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "idea-darkula-theme" "20160416.1603" "Color theme based on IntelliJ IDEA Darkula color theme" '((emacs "24.1")) :url "http://github.com/fourier/idea-darkula-theme" :keywords '("themes"))
